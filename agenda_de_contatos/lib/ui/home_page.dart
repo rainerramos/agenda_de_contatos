e7d601110ee18c83bf48fce98dfeb9d802374d23
Rainer Ramos <rainermr89@hotmail.com>
2021-04-19 15:43:49 -0300

    Criando um Botão Flutuante

--- agenda_de_contatos/lib/ui/home_page.dart
+++ agenda_de_contatos/lib/ui/home_page.dart
@@ -11,26 +11,40 @@ class _HomePageState extends State<HomePage> {
 
   ContactHelper helper = ContactHelper();
 
+  List<Contact> contacts = List();
 
   @override
   void initState() {
     super.initState();
 
-    Contact c = Contact();
-    c.name  = "Marcos Ramos";
-    c.email = "marcos@hotmail.com";
-    c.phone = "4367841738";
-    c.img = "imgtest";
-
-    helper.saveContact(c);
-
     helper.getAllContatcts().then((list) {
-      print(list);
+      setState(() {
+        contacts = list;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors. red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          
+        }
+      ),
+    );
   }
 }
