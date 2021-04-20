c59394a379b2d20303a3f8004bccd6ee0ff035c4
Rainer Ramos <rainermr89@hotmail.com>
2021-04-20 11:48:11 -0300

    Layout da Tela de Contato

--- agenda_de_contatos/lib/helpers/contact_helper.dart
+++ agenda_de_contatos/lib/helpers/contact_helper.dart
@@ -9,7 +9,6 @@ final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
 class ContactHelper {
-
   static final ContactHelper _instance = ContactHelper.internal();
 
   factory ContactHelper() => _instance;
@@ -19,7 +18,7 @@ class ContactHelper {
   Database _db;
 
   Future<Database> get db async {
-    if(_db != null){
+    if (_db != null) {
       return _db;
     } else {
       _db = await initDb();
@@ -30,14 +29,13 @@ class ContactHelper {
   Future<Database> initDb() async {
     final databasesPath = await getDatabasesPath();
     final path = join(databasesPath, "contactsnew.db");
-    
-    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async {
-      await db.execute(
-        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT,"
-            "$phoneColumn TEXT, $imgColumn TEXT)"
-      );
-    });
 
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT,"
+          "$phoneColumn TEXT, $imgColumn TEXT)");
+    });
   }
 
   Future<Contact> saveContact(Contact contact) async {
@@ -49,10 +47,10 @@ class ContactHelper {
   Future<Contact> getContact(int id) async {
     Database dbContact = await db;
     List<Map> maps = await dbContact.query(contactTable,
-    columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
-    where: "$idColumn = ?",
-    whereArgs: [id]);
-    if(maps.length > 0) {
+        columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+        where: "$idColumn = ?",
+        whereArgs: [id]);
+    if (maps.length > 0) {
       return Contact.fromMap(maps.first);
     } else {
       return null;
@@ -61,13 +59,14 @@ class ContactHelper {
 
   Future<int> deleteContact(int id) async {
     Database dbContact = await db;
-    return await dbContact.delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
+    return await dbContact
+        .delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
   }
 
-  Future<int> updateContact (Contact contact) async {
+  Future<int> updateContact(Contact contact) async {
     Database dbContact = await db;
-    return await dbContact.update(contactTable, contact.toMap(), where: "$idColumn = ?",
-        whereArgs: [contact.id]);
+    return await dbContact.update(contactTable, contact.toMap(),
+        where: "$idColumn = ?", whereArgs: [contact.id]);
   }
 
   Future<List> getAllContatcts() async {
@@ -79,21 +78,20 @@ class ContactHelper {
     }
     return listContact;
   }
-  
+
   Future<int> getNumber() async {
     Database dbContact = await db;
-    return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
+    return Sqflite.firstIntValue(
+        await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
   }
 
   Future close() async {
     Database dbContact = await db;
     dbContact.close();
   }
-
 }
 
 class Contact {
-
   int id;
   String name;
   String email;
@@ -110,14 +108,14 @@ class Contact {
     img = map[imgColumn];
   }
 
-  Map toMap(){
-    Map<String, dynamic> map ={
+  Map toMap() {
+    Map<String, dynamic> map = {
       nameColumn: name,
       emailColumn: email,
       phoneColumn: phone,
       imgColumn: img
     };
-    if(id != null) {
+    if (id != null) {
       map[idColumn] = id;
     }
     return map;
